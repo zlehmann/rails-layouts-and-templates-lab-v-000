6d2e6fc181decd0706f5f2fe356b9e0144380378
Zachary Lehmann <zlehmann@gmail.com>
2019-01-29 20:39:03 +0000

    Automatically backed up by Learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,9 +1,14 @@
 class StoreAdminController < ApplicationController
+  layout "admin"
+  
   def home
-    render :layout => "admin"
   end
 
   def orders
     render :layout => "order_administration"
+  end
+
+  def invoice 
+    
   end
 end
